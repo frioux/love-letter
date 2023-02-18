77369964833b007a1f0b1b29fdb95f000e6804d1
Arthur Axel 'fREW' Schmidt <git@frew.co>
2023-02-18 13:59:36 -0800

    add spinner due to slow loading

--- fe/src/lib/Screen.svelte
+++ fe/src/lib/Screen.svelte
@@ -10,9 +10,48 @@
    }, 550)
 </script>
 
-Rendering: {s}<br>
-<br>
-<img alt="render of {s}" src="/render/?s={s}" />
+<style>
+.my-img-container {
+  position: relative;
+  padding-top: 50%;
+}
+.my-img-container:before {
+  content: " ";
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 80px;
+  height: 80px;
+  border: 2px solid white;
+  border-color: transparent white transparent white;
+  border-radius: 50%;
+  animation: loader 1s linear infinite;
+}
+.my-img-container > img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100% !important;
+  height: 100% !important;
+}
+@keyframes loader {
+  0% {
+    transform: translate(-50%,-50%) rotate(0deg);
+  }
+  100% {
+    transform: translate(-50%,-50%) rotate(360deg);
+  }
+}
+</style>
+
+
+<div class="my-img-container">
+   {#key s}
+      <img alt="Rendering '{s}'" src="/render/?s={s}" />
+   {/key}
+</div>
 
 <form action="/save/" method="POST">
   <input on:input={handleInput} name="s" />
