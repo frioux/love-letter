9577ace551afa4c1eac4bf15a21756e206d1b4e5
Arthur Axel 'fREW' Schmidt <git@frew.co>
2023-02-21 09:53:17 -0800

    document source of CSS

--- fe/src/lib/Screen.svelte
+++ fe/src/lib/Screen.svelte
@@ -11,39 +11,40 @@
 </script>
 
 <style>
-.my-img-container {
-  position: relative;
-  padding-top: 50%;
-}
-.my-img-container:before {
-  content: " ";
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  width: 80px;
-  height: 80px;
-  border: 2px solid white;
-  border-color: transparent white transparent white;
-  border-radius: 50%;
-  animation: loader 1s linear infinite;
-}
-.my-img-container > img {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  width: 100% !important;
-  height: 100% !important;
-}
-@keyframes loader {
-  0% {
-    transform: translate(-50%,-50%) rotate(0deg);
+  /* Via https://stackoverflow.com/a/45019339 */
+  .my-img-container {
+    position: relative;
+    padding-top: 50%;
   }
-  100% {
-    transform: translate(-50%,-50%) rotate(360deg);
+  .my-img-container:before {
+    content: " ";
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 80px;
+    height: 80px;
+    border: 2px solid white;
+    border-color: transparent white transparent white;
+    border-radius: 50%;
+    animation: loader 1s linear infinite;
   }
-}
+  .my-img-container > img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100% !important;
+    height: 100% !important;
+  }
+  @keyframes loader {
+    0% {
+      transform: translate(-50%,-50%) rotate(0deg);
+    }
+    100% {
+      transform: translate(-50%,-50%) rotate(360deg);
+    }
+  }
 </style>
 
 
